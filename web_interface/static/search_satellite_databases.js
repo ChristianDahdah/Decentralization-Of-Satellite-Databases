cfc73188b2d36ef15a5f9facce869532f792c346
Dahdah Christian <alcheikhchristian.eldahdah@cnes.fr>
2021-08-23 14:49:57 +0000

    Improved databases search (added row ID)

--- web_interface/static/search_satellite_databases.js
+++ web_interface/static/search_satellite_databases.js
@@ -1,21 +1,18 @@
-$(function(){
+$('#search_databases').click(function() {
 
-    $('#search_databases').click(function() {
-    
-        $.ajax({
-            type: "POST",
-            url: "/satellite_demo/search_satellite_databases",
-            data: { 
-                'satellite_id' : $('#satellite_id').val(),
-                'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
-            },
-            success: searchSuccess,
-            dataType: 'html'
-        });
-        
-    });
+$.ajax({
+	type: "POST",
+	url: "/satellite_demo/search_satellite_databases",
+	data: { 
+	'satellite_id' : $('#satellite_id').val(),
+	'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
+	},
+	success: searchSuccess,
+	dataType: 'html'
+	});
 
 });
+
 
 function searchSuccess(data, textStatus, jqXHR)
 {
